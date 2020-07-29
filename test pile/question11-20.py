0597324a92c4100803b00cc5209434e5d7f87a4a
ciel-star <top010817@naver.com>
2020-07-29 19:33:12 +0900

    hello

---
+++ test pile/question11-20.py
@@ -0,0 +1,39 @@
+삼성전자 = 50000
+총평가금액 = 삼성전자*10
+print(총평가금액)
+print()
+
+시가총액 = 2980000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+s = "hello"
+t = "python"
+print(s+"!",t)
+
+print(2+2*3)
+
+a = 132
+print(type(a))
+
+num_str = "720"
+print(int(num_str), type(num_str))
+
+num = 100
+result = str(num)
+print(result, type(result))
+
+data = 15.79
+data = float(data)
+print(data, type(data))
+
+year = "2020"
+year = int(year)
+print(year-1,year,year+1)
+
+월 = 48584
+총금액 = 월*36
+print(총금액)
