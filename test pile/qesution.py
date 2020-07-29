39baa285f2edf32e623f4a9d9573a4208924c629
ciel-star <top010817@naver.com>
2020-07-29 17:21:20 +0900

    hello

---
+++ test pile/qesution.py
@@ -0,0 +1,17 @@
+print("hello world\n")
+
+print("Mary's cosmetics\n")
+
+print('신씨가 소리질렀다. "도둑이야".\n')
+
+print("C:\Windows\n")
+
+print("안녕하세요.\n만나서\t\t반갑습니다.\n")
+
+print("naver","kakao","sk","smasung\n", sep=";")
+
+print("naver","kakao","sk","samsung\n", sep="/")
+
+print("first", end="");print("second\n")
+
+print(5/3)
